cea51db80953ce1b92857a83978fea8dc1489037
PimMiii <pim.milt@gmail.com>
2022-10-11 20:35:47 +0200

    added default policies to Beer and Brewers

--- framework/app/Http/Controllers/BeerController.php
+++ framework/app/Http/Controllers/BeerController.php
@@ -8,19 +8,29 @@ use Illuminate\Support\Carbon;
 
 class BeerController extends Controller
 {
+    public function __construct()
+    {
+        $this->authorizeResource(Beer::class, Beer::class);
+    }
+
+
     public function index() {
         $beers = Beer::all();
         return view('beers.index', compact('beers'));
     }
+
 
     public function show($id) {
         $beer = Beer::find($id);
         return view('beers.show', compact('beer'));
     }
 
+
     public function create() {
         return view('beers.create');
     }
+
+
     public function store(Request $request){
         //percentage needs to be adjusted for database. [percentage * 100]
         $data = $request->all();
@@ -35,6 +45,6 @@ class BeerController extends Controller
                 'description' => 'nullable'
             ]);
         Beer::create($request->all());
-        return redirect('/beers/');
+        return redirect(route('beers.index'));
     }
 }
--- framework/app/Http/Controllers/BrewersController.php
+++ framework/app/Http/Controllers/BrewersController.php
@@ -7,23 +7,23 @@ use Illuminate\Http\Request;
 
 class BrewersController extends Controller
 {
+    public function __construct()
+    {
+        $this->authorizeResource(Brewer::class, Brewer::class);
+    }
+
     public function index()
     {
         $brewers = Brewer::all();
         return view('brewers.index', compact('brewers'));
-
     }
 
-    public function show($id)
-    {
-        $brewer = Brewer::find($id);
-        return view('brewers.show', compact('brewer'));
-    }
 
     public function create()
     {
         return view('brewers.create');
     }
+
 
     public function store(Request $request)
     {
@@ -34,6 +34,41 @@ class BrewersController extends Controller
                 'description' => 'nullable'
             ]);
         Brewer::create($request->all());
-        return redirect('/brewers/');
+        return redirect(route('brewers.index'));
     }
+
+
+    public function show($id)
+    {
+        $brewer = Brewer::find($id);
+        return view('brewers.show', compact('brewer'));
+    }
+
+    public function update(Request $request)
+    {
+        $validated = $this->validate($request,
+            [
+                'id' => 'bail|required|exists:categories',
+                'name' => 'bail|required|unique:categories|max:255',
+                'description' => 'nullable'
+            ]);
+        $category = Brewer::find($validated['id']);
+        $category->name = $validated['name'];
+        $category->description = $validated['description'];
+        $category->save();
+        return redirect(route('brewers.index'));
+    }
+
+
+    public function destroy(Request $request)
+    {
+        $validated = $this->validate($request,
+            [
+                'id' => 'bail|required|exists:categories'
+            ]);
+        Brewer::destroy($validated['id']);
+        return redirect(route('brewers.index'));
+    }
+
+
 }
