34282c4cbd3631b3c6cdb00f0c3287d725a6b7f6
PimMiii <pim.milt@gmail.com>
2022-10-24 23:14:52 +0200

    migration nullable fix

--- framework/database/migrations/2022_09_29_143826_add_beer_id_to_reviews_table.php
+++ framework/database/migrations/2022_09_29_143826_add_beer_id_to_reviews_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('reviews', function (Blueprint $table) {
-            $table->foreignId('beer_id')->after('id')->constrained()->nullOnDelete();
+            $table->foreignId('beer_id')->after('id')->nullable()->constrained()->nullOnDelete();
         });
     }
 
