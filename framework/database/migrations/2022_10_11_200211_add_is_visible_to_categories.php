175768ffcf2539818b46b88e65447810fe58e790
PimMiii <pim.milt@gmail.com>
2022-10-26 14:19:13 +0200

    changed is_hidden to is_visible, to make more sense with toggles

--- framework/database/migrations/2022_10_11_200211_add_is_visible_to_categories.php
+++ framework/database/migrations/2022_10_11_200211_add_is_visible_to_categories.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('categories', function (Blueprint $table) {
-            $table->boolean('is_hidden')->default(0);
+            $table->boolean('is_visible')->default(1);
         });
     }
 
--- framework/database/migrations/2022_10_11_200300_add_is_visible_to_beers.php
+++ framework/database/migrations/2022_10_11_200300_add_is_visible_to_beers.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('beers', function (Blueprint $table) {
-            $table->boolean('is_hidden')->default(0);
+            $table->boolean('is_visible')->default(1);
         });
     }
 
