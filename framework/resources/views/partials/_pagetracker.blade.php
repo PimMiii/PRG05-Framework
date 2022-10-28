4101dd1087e83586e630fce5f1e7f7bd9a1b079c
PimMiii <pim.milt@gmail.com>
2022-10-28 18:13:23 +0200

    layout updating

--- framework/resources/views/partials/_pagetracker.blade.php
+++ framework/resources/views/partials/_pagetracker.blade.php
@@ -7,16 +7,16 @@
 
     @endphp
     @if($i >= 5 and !Auth::user()->is_verified)
-        <div class="alert alert-success" role="alert">
+        <div class="alert alert-info" role="alert">
             <h4 class="alert-heading">Goed Gedaan</h4>
             <p>Uw account voldoet nu aan de eisen om te verifiëren!</p>
             <hr>
 
 
-        <a href="{{ route('profile.update-verified', Auth::id()) }}"
+        <a class="btn btn-success" href="{{ route('profile.update-verified', Auth::id()) }}"
            onclick="event.preventDefault();
                                                      document.getElementById('verify-form').submit();">
-            Verifieer account nu!
+            Account verifiëren!
         </a>
 
         <form id="verify-form" action="{{ route('profile.update-verified', Auth::id()) }}" method="POST" class="d-none">
